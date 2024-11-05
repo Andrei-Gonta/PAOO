22dce3a1b6cb817a8931e8df6e4de34d772bb785
Andrei Gonta <gontaandrei02@gmail.com>
2024-11-06 00:09:20 +0200

    ex4

--- Tema_1.cpp
+++ Tema_1.cpp
@@ -1,28 +1,42 @@
+
 #include <iostream>
 #include <string>
 using namespace std;
 
 class Car {
     private:
-        string brand;
-        string model;
-        int engine_capacity;
-        int engine_power;
+        string* brand;
+        string* model;
+        int* engine_capacity;
+        int* engine_power;
         
     
     
     public: 
-        Car(string& brand, int engine_capacity, int engine_power) 
-        : brand(brand), engine_capacity(engine_capacity), engine_power(engine_power)
+        Car(const string& brand,const string& model,int engine_capacity, int engine_power) 
+        //: brand(brand),  model(model), engine_capacity(engine_capacity), engine_power(engine_power)
         {
-            cout<<"Car: "<<brand<<" "<<model<<"created successfully!"<<endl;
+            this->brand=new string(brand);
+            this->model=new string(model);
+            this->engine_capacity=new int(engine_capacity);
+            this->engine_power=new int(engine_power);
+            
+            cout<<"Car: "<<brand<<" "<<model<<" created successfully!"<<endl;
         }
        
         
         void apply_soft_power(int hp)
         {
-            engine_power=engine_power+hp;
-            cout<<"Now your car has :"<<engine_power<<"hp!"<<endl;
+            *engine_power+=hp;
+            cout<<"Now your car has :"<<*engine_power<<"hp!"<<endl;
+        }
+        
+        ~Car()
+        {
+            delete brand;
+            delete model;
+            delete engine_capacity;
+            delete engine_power;
         }
         
         
@@ -34,7 +48,8 @@ class Car {
 
 int main()
 {
-   cout<<"Hello World";
+    Car car("VW", "Golf", 1600, 110);
+    car.apply_soft_power(30);
 
     return 0;
 }
