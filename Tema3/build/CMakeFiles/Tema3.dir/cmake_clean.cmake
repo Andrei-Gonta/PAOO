1f6b18a2e519cba6144641015b2de877fd89bb63
Andrei Gonta <gontaandrei02@gmail.com>
2024-12-03 18:10:14 +0000

    cmake done

---
+++ Tema3/build/CMakeFiles/Tema3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema3.dir/link.d"
+  "CMakeFiles/Tema3.dir/src/main.cpp.o"
+  "CMakeFiles/Tema3.dir/src/main.cpp.o.d"
+  "Tema3"
+  "Tema3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
